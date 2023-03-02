2ea08125e80d2b55fd4a300fac5f222c7eec7001
Artyom Tonoyan <arttonoyan@gmail.com>
2023-03-02 20:21:31 +0400

    Add IsDeleted property fro blog

--- src/Training.MultiTenancy.Data/Entities/Blog.cs
+++ src/Training.MultiTenancy.Data/Entities/Blog.cs
@@ -2,12 +2,13 @@
 
 namespace Training.MultiTenancy.Data.Entities;
 
-public class Blog : ITenantBaseEntity
+public class Blog : ITenantBaseEntity, IBaseEntity
 {
     public int TenantId { get; set; }
     public int BlogId { get; set; }
     public string Name { get; set; } = null!;
     public string Url { get; set; } = null!;
+    public bool IsDeleted { get; set; }
 
     public List<Post> Posts { get; set; } = null!;
 }
